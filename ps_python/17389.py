bb9154d87b387a5ba273e1ad55133a352ae075be
DESKTOP-CS5B0E0\i <>
2020-08-06 10:33:18 +0900

    2020.08.06 17389_보너스점수_구현

--- ps_python/17389.py
+++ ps_python/17389.py
@@ -3,15 +3,14 @@ n = int(input())
 l = list(map(ord,list(input())))
 # O(알파벳 대문자 O, ASCII 코드 79)와 X(알파벳 대문자 X, ASCII 코드 88)
 
-# print(l)
 b_flag = 1 # flag
 bonus = 0
 total = 0
 for idx,i in enumerate(l,1):
     if i == 79:
-        total = total + idx
-        bonus = bonus + 1
+        total = total + idx + bonus
+        bonus += 1
     else:
         bonus = 0
 
-print(total,bonus)
+print(total)

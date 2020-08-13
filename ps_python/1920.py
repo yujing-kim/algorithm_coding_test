ee0220000c370c2701afe73431736272a8a00673
dev-yujin <kyouj96@gmail.com>
2020-08-13 11:23:11 +0900

    2020.08.13 1920.py _ 구현

---
+++ ps_python/1920.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline())
+l = set(map(int,sys.stdin.readline().split()))
+# print(l)
+
+num = int(sys.stdin.readline())
+num_l = list(map(int,sys.stdin.readline().split()))
+# print(num_l)
+
+for i in num_l:
+    if i in l:
+        print(1)
+    else :
+        print(0)
